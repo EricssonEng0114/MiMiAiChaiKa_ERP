9e9cf1db3de7aaac9e9abdc71fe34e6cc012710b
Ericsson <ericsson0114@gmail.com>
2024-07-08 01:34:45 +0800

    remove API Function form common.service.ts to API.service.ts

--- src/app/Services/Common/common.service.ts
+++ src/app/Services/Common/common.service.ts
@@ -1,12 +1,13 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CommonService {
-  private apiUrl = 'https://localhost:44398/';
-  private sessionName = 'webAPI_accessToken';
-  private apiKey = '4kHn9zM?dHfuEYf5+y%NaqG&Q7FnR8';
 
-  constructor() { }
+
+  constructor(private http: HttpClient) { }
+
+  
 }
