7931447086c9419b9790a9b7a6c34a487cd25474
Ericsson <ericsson0114@gmail.com>
2024-07-10 01:50:44 +0800

    Revert "remove API Function form common.service.ts to API.service.ts"
    This reverts commit 9e9cf1db3de7aaac9e9abdc71fe34e6cc012710b.

--- src/app/Services/Common/common.service.ts
+++ src/app/Services/Common/common.service.ts
@@ -1,13 +1,12 @@
-import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CommonService {
+  private apiUrl = 'https://localhost:44398/';
+  private sessionName = 'webAPI_accessToken';
+  private apiKey = '4kHn9zM?dHfuEYf5+y%NaqG&Q7FnR8';
 
-
-  constructor(private http: HttpClient) { }
-
-  
+  constructor() { }
 }
