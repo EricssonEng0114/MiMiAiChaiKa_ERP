306f4ae4c008943668086e7eb0a6f95ab7ec103b
Ericsson <ericsson0114@gmail.com>
2024-07-10 03:01:36 +0800

    Add in html for the login using Angular Materials & Bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatCard, MatCardModule } from '@angular/material/card';
 import { LoginComponent } from './features/login/login.component';
+import { provideHttpClient, withFetch } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -13,9 +18,18 @@ import { LoginComponent } from './features/login/login.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatInputModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatCardModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [
+    provideHttpClient(
+      withFetch()
+    ),
     provideAnimationsAsync()
   ],
   bootstrap: [AppComponent]
