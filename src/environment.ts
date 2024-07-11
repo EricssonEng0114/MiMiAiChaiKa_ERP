a849ff612279ac7c68b3cbd2dd664be509d2d478
Ericsson <ericsson0114@gmail.com>
2024-07-11 21:02:40 +0800

    Add in environment, setup login services & add in Web API Key Model

---
+++ src/environment.ts
@@ -0,0 +1,6 @@
+export const environment = {
+    production: false,
+    apiUrl: 'https://localhost:44378/',
+    dataDecryptKey: "MFwwDQYJKoZIhvcNAQEBBQADSwAwSAJBAJE+vHY2xwmBxFvIAJpC7KsO9zN8PoC2wE0fXD+2/IkaRJb4XFl8BapL6pBgYk0xER5onLQC8ehRHkLUdiJ4FgECAwEAAQ=="  // Example API endpoint
+    // Add other environment-specific variables here
+};
